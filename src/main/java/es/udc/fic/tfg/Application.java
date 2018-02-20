198e1ccb19be55de90883a447963104bd4a1fac3
p.juane <p.juane@udc.es>
2018-02-20 17:28:38 +0100

    Initial commit

---
+++ src/main/java/es/udc/fic/tfg/Application.java
@@ -0,0 +1,3 @@
+package es.udc.fic.tfg;
+
+public interface Application {}
---
+++ src/main/java/es/udc/fic/tfg/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package es.udc.fic.tfg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import es.udc.fic.tfg.Application;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
