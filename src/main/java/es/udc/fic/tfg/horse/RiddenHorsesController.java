44da559e193b306a4d6e5afaa11229b0afc282ae
p.juane <p.juane@udc.es>
2018-07-09 21:50:11 +0200

    Paginacion añadida en riddenHorses

--- src/main/java/es/udc/fic/tfg/horse/RiddenHorsesController.java
+++ src/main/java/es/udc/fic/tfg/horse/RiddenHorsesController.java
@@ -1,9 +1,13 @@
 package es.udc.fic.tfg.horse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import es.udc.fic.tfg.account.Account;
 import es.udc.fic.tfg.account.AccountService;
@@ -20,14 +24,33 @@ public class RiddenHorsesController {
     
     @Autowired
     private AccountService accountService;
+    
+    @Autowired
+    private HorseRepository horseRepository;
 
     @GetMapping("riddenHorses")
     // @ResponseStatus(value = HttpStatus.OK)
     //@Secured({"ROLE_USER", "ROLE_ADMIN"})
     public String riddenHorses(Model m, Principal principal) {
-    	 Account owner = accountService.findByEmail(principal.getName());
+    	 Account rider = accountService.findByEmail(principal.getName());
          
-    	m.addAttribute("horses", owner.getHorsesRidden());
+    	 Pageable pageable = new PageRequest(0,2,Sort.Direction.DESC,"nickname");
+         
+     	m.addAttribute("horses", horseRepository.findByRider(rider,pageable));
+    	 
+    	//m.addAttribute("horses", owner.getHorsesRidden());
+        return RIDDENHORSES_VIEW_NAME;
+    }
+    
+    @GetMapping("riddenHorses/{page}")
+    // @ResponseStatus(value = HttpStatus.OK)
+    //@Secured({"ROLE_USER", "ROLE_ADMIN"})
+    public String riddenHorses(Model m, Principal principal, @PathVariable("page") int pageNum) {
+        Account rider = accountService.findByEmail(principal.getName());
+
+        Pageable pageable = new PageRequest(pageNum,2,Sort.Direction.DESC,"nickname");
+
+        m.addAttribute("horses", horseRepository.findByRider(rider,pageable));
         return RIDDENHORSES_VIEW_NAME;
     }
 
